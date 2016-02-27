3118ab0a58a4d253b3034a602bb01957445f79b7
Dennis Lysenko <dennis.s.lysenko@gmail.com>
2016-02-27 15:14:57 -0500

    make the render_to_video.sh script take whatever is in test.flam3 and make a video from it

--- render_to_video.sh
+++ render_to_video.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-# assuming you just generated a bunch of frames with the filenames 00000.png, 00001.png, 00002.png etc.
-ffmpeg -y -framerate 30 -i %05d.png -c:v libx264 -r 30 -pix_fmt yuv420p out.mp4 && open out.mp4
+rm *.png
+
+flam3-render < test.flam3 && ffmpeg -y -framerate 30 -i %05d.png -c:v libx264 -r 30 -pix_fmt yuv420p out.mp4 && open out.mp4
